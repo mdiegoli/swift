69d25fa13c9bd9bb1a59aed043b016dc71f6de57
Michele <michelediegoli123@gmail.com>
2024-07-07 23:10:38 +0200

    Update arrayOfSizes.swift

--- arrayOfSizes.swift
+++ arrayOfSizes.swift
@@ -2,7 +2,7 @@ import Foundation
 
 var taglieStr: [String] = []
 var taglieNum: [String] = []
-var taglieMist:[String] = []
+var taglieMist:[String:[String]] = [:]
 let taglie = ["S", "43", "XL", "40", "44", "M", "12", "IT 35", "IT 43", "FR 12", "UK 50", "XXL", "IT 50"]
 
 func toInt(_ taglia:String) -> Int?
@@ -15,12 +15,28 @@ taglie.forEach{ t in
     taglieNum.append(t)
   } else {
       if t.contains(" "){
-        taglieMist.append(t)
+        let tokens = t.components(separatedBy: " ")
+        if taglieMist.keys.contains(tokens[0]) {
+          var arrayToAppend = taglieMist[tokens[0]]
+          arrayToAppend?.append(tokens[1])
+          taglieMist[tokens[0]] = arrayToAppend
+        }else{
+          let arrayToAppend = [tokens[1]]
+          taglieMist[tokens[0]] = arrayToAppend
+        }
       }else{
         taglieStr.append(t)
       }
   }
 }
-print(taglieStr)
-print(taglieNum)
-print(taglieMist)
+print("Taglie formato testo")
+print(taglieStr.sorted())
+print("Taglie formato numerico")
+print(taglieNum.sorted())
+print("Taglie formato misto")
+for (k,v) in taglieMist {
+  print("Nazione")
+  print(k)
+  print("Taglie")
+  print(v.sorted())
+}
